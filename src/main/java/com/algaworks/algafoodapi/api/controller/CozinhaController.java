207df2ba9b92292fbc25353c37b28bbe4e03cef6
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-19 19:25:05 -0300

    Configuring controllers to handle both xml and json requests.

--- src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -3,7 +3,9 @@ package com.algaworks.algafoodapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,7 @@ import com.algaworks.algafoodapi.domain.model.Cozinha;
 import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 public class CozinhaController {
 
     @Autowired
@@ -20,6 +22,13 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping("{cozinhaId}")
+    // o @PathVariable também aceita o bind como parâmetro. Ex.:
+    // @PathVariable("cozinhaId") Long id
+    public Cozinha buscar(@PathVariable Long cozinhaId) {
+        return cozinhaRepository.buscar(cozinhaId);
     }
 
 }
--- src/main/java/com/algaworks/algafoodapi/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/EstadoController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafoodapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import com.algaworks.algafoodapi.domain.model.Estado;
 import com.algaworks.algafoodapi.domain.repository.EstadoRepository;
 
 @RestController
-@RequestMapping("estados")
+@RequestMapping(value = "estados", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 public class EstadoController {
 
     @Autowired
