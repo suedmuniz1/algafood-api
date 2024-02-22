02bfe4e5a2b3870645e8ebc1dfe12b28beb25558
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-22 16:08:00 -0300

    Updating values being inserted into Restaurantes table by default.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,9 +2,9 @@ insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
 insert into cozinha (nome) values ('Brasileira');
 
-insert into restaurante (nome, taxa_frete, cozinha_id) values ("Thai gourmet", "10", 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ("Thai delivery", "9.50", 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ("Tuk Tuk comida indiana", "15", 2);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ("Thai gourmet", 10, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ("Thai delivery", 9.50, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ("Tuk Tuk comida indiana", 15, 2);
 
 insert into forma_pagamento (descricao) values ('Crédito');
 insert into forma_pagamento (descricao) values ('Débito');
