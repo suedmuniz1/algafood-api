b876fa70597c7715863de23536d79173cd1ff80c
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-29 17:41:39 -0300

    Removing unused imports.

--- src/main/java/com/algaworks/algafoodapi/domain/repository/CustomJpaRepository.java
+++ src/main/java/com/algaworks/algafoodapi/domain/repository/CustomJpaRepository.java
@@ -1,10 +1,9 @@
 package com.algaworks.algafoodapi.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @NoRepositoryBean
 public interface CustomJpaRepository<T, ID> extends JpaRepository<T, ID> {
